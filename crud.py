77d0fbe8de79f83de0f64c14d06924fc02610ec5
Kestutis Januskevicius <infohata@gmail.com>
2022-10-18 12:16:45 +0300

    abstract UD

--- crud.py
+++ crud.py
@@ -15,26 +15,6 @@ def read_tevai():
     tevai = session.query(Tevas).all()
     return tevai
 
-def update_tevas(tevas_id, **kwargs):
-    tevas = session.query(Tevas).get(tevas_id)
-    if tevas:
-        if "vardas" in kwargs:
-            tevas.vardas = kwargs["vardas"]
-        if "pavarde" in kwargs:
-            tevas.pavarde = kwargs["pavarde"]
-        session.commit()
-    else:
-        print(f"Klaida: Tevas su ID:{tevas_id} neegzistuoja")
-
-def delete_tevas(tevas_id):
-    tevas = session.query(Tevas).get(tevas_id)
-    if tevas:
-        session.delete(tevas)
-        session.commit()
-        return True
-    else:
-        print(f"Klaida: Tevas su ID:{tevas_id} neegzistuoja")
-
 def create_vaikas(vardas, pavarde, tevas, mokymo_istaiga=None):
     vaikas = Vaikas(
         vardas=vardas, 
@@ -49,21 +29,41 @@ def create_vaikas(vardas, pavarde, tevas, mokymo_istaiga=None):
 def read_vaikai():
     return session.query(Vaikas).all()
 
+def delete_object(object_class, object_id):
+    obj = session.query(object_class).get(object_id)
+    if obj:
+        session.delete(obj)
+        session.commit()
+        return True
+    else:
+        print(f"Klaida: {object_class.__name__} su ID:{object_id} neegzistuoja")
+
+def update_object(object_class, object_id, **kwargs):
+    obj = session.query(object_class).get(object_id)
+    if obj and kwargs:
+        for column_name, value in kwargs.items():
+            if hasattr(obj, column_name):
+                setattr(obj, column_name, value)
+            else:
+                print(f"Klaida: {obj} neturi {column_name} atributo")
+        else:
+            session.commit()
+            return obj
+    else:
+        print(f"Klaida: {object_class.__name__} su ID:{object_id} neegzistuoja")
+
 # Testuojam
 if __name__ == "__main__":
-    # - Naujas Vaikas
-    # tevas = session.query(Tevas).get(1)
-    # naujas_vaikas = create_vaikas(
-    #     "Emilija", "Januskeviciute", 
-    #     tevas, "Scuola di Staggia Sienese"
-    # )
-    # print(read_tevai())
-    print(read_vaikai())
-    # - Naujas Tevas
-    # naujas_tevas = create_tevas("Kestutis", "Januskevicius")
-    # print(naujas_tevas.id, naujas_tevas.vardas, naujas_tevas.pavarde)
-    # - Atnaujintas Tevas
-    # update_tevas(1, vardas="Geras", pavarde="Programuotojas")
-    # update_tevas(1, vardas="Neblogas")
-    # - Trinamas Tevas
-    # print(delete_tevas(1))
+    # --- Ivaikinimo scenarijus
+    vaikas = session.query(Vaikas).filter(Vaikas.pavarde.ilike("Super%")).first()
+    tevas = session.query(Tevas).filter(Tevas.pavarde.ilike("Jan%")).first()
+    ivaikintas = update_object(Vaikas, vaikas.id, tevas=tevas, vardas="Ivaikintas")
+    print("Python objektas `ivaikintas`:\n", vaikas)
+    print("Perkraunam is DB:\n", read_vaikai())
+
+    # --- Sukuriam superine seima, ir nutrinam teva
+    # naujas_tevas = create_tevas("Tevas", "Superinis")
+    # naujas_vaikas = create_vaikas("Vaikas", "Superinis", naujas_tevas)
+    # print(read_vaikai())
+    # delete_object(Tevas, naujas_tevas.id)
+    # print(read_vaikai())
